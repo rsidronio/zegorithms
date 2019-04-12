43669cd45e499b08c5093644200351153ceea799
Eduardo Trindade <eduardotrinn@gmail.com>
2019-04-12 15:25:36 -0300

    finalizando parsers

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
 #!/usr/bin/env node
 const program = require('commander');
-const { simpleParse, apply } = require('./romero');
+const { parse, simpleParse, apply, applyCompute } = require('./romero');
 
 program 
-    .version('1.0.0')
-    .description('parser simples');
+  .version('1.0.2')
+  .description('romero parsers');
 
 program
-    .command('simpleParse <input>')
-    .alias('p')
-    .description('calcula a entrada <string> e retorna um inteiro')
-    .action((input) => {
-      console.log(simpleParse(input));
-    });
+  .command('parse <input>')
+  .alias('p')
+  .description('dada uma equação <string> retorna sua forma simplicada para fácil cálculo')
+  .action((input) => {
+    console.log(parse(input));
+  });
 
 program
-    .command('apply <a> <b> <op> ')
-    .alias('a')
-    .description('aplica um dado operador em dada duas entradas')
-    .action((a, b, op) => {
-      console.log(apply(a,b,op));
-    });
+  .command('simpleParse <input>')
+  .alias('s')
+  .description('calcula a entrada <string> e retorna um float')
+  .action((input) => {
+    console.log(simpleParse(input));
+  });
+
+program
+  .command('apply <a> <b> <op> ')
+  .alias('a')
+  .description('aplica um dado operador em dada duas entradas')
+  .action((a, b, op) => {
+    console.log(apply(a,b,op));
+  });
+
+
+program
+  .command('compute <simpl>')
+  .alias('c')
+  .description('dada uma lista na forma simplicada retorna o resultado desta equação')
+  .action((simpl) => {
+    console.log(applyCompute(simpl));
+  });
 
 program.parse(process.argv);
--- js/romero.js
+++ js/romero.js
@@ -1,142 +1,236 @@
     
+/**
+ * E -> (E)
+ * 
+ * E -> E O E
+ * 
+ * E -> N
+ * 
+ * N -> {0-9}+
+ * 
+ * O -> + | - | * | /
+ * 
+ * @param {string} input E
+ * @returns {number} N
+ * @author zeduado <jets(a)cin.ufpe.br> 
+ */
+const parse = (input) => {
+    input = input.trim();
+    let stack = [];
+    let simplified = [];
 
-    /**
-     * E -> ( E O E )
-     * 
-     * E -> N
-     * 
-     * O -> + | - | * | /
-     * 
-     * N -> {0-9}+
-     * 
-     * @param {string} input E
-     * @returns {number} N
-     * @author zeduado <jets(a)cin.ufpe.br>
-     */
-    const simpleParse = (input) => {
-        input = input.trim();
-        let stack = [];
+    for(let i = 0; i < input.length; i++) {
+        let curr = input[i];
+        if(isNumber(curr))
+            simplified.push(curr);
+        else if (isOperator(curr))
+            reduceOperator(curr, stack, simplified);
+        else if(isOpen(curr))
+            stack.push(curr);
+        else if(isClose(curr))
+            reduce(stack, simplified);
+        else if(!isWhiteSpace(curr))
+            throw Error("token invalido");
+    }
 
-        for(let i = 0; i < input.length; i++) {
-            let curr = input[i];
-            if (isClose(curr)) {
-                stack.push(curr);
-                stack.push(simpleReduce(stack));
-            } else if (isNumber(curr)) {
-                let complete = input.floatAt(i);
-                stack.push(complete.get);
-                i = complete.lastIndex;
-            } else if (isOperator(curr) || isOpen(curr)) {
-                stack.push(curr);
-            } else if (!isWhiteSpace(curr))
-                throw Error("token invalido");
+    simplified = simplified.concat(stack);
+    return compute(simplified);
+}
 
-            console.log(i, stack);
+/**
+ * E -> ( E O E )
+ * 
+ * E -> N
+ * 
+ * O -> + | - | * | /
+ * 
+ * N -> {0-9}+
+ * 
+ * @param {string} input E
+ * @returns {number} N
+ * @author zeduado <jets(a)cin.ufpe.br>
+ */
+const simpleParse = (input) => {
+    input = input.trim();
+    let stack = [];
+
+    for(let i = 0; i < input.length; i++) {
+        let curr = input[i];
+        if (isClose(curr)) {
+            stack.push(curr);
+            stack.push(simpleReduce(stack));
+        } else if (isNumber(curr)) {
+            let complete = input.floatAt(i);
+            stack.push(complete.get);
+            i = complete.lastIndex;
+        } else if (isOperator(curr) || isOpen(curr)) {
+            stack.push(curr);
+        } else if (!isWhiteSpace(curr))
+            throw Error("token invalido");
+
+        console.log(i, stack);
+    }
+
+    if (stack.length == 1 ) return stack[0];
+    else throw Error("alguma coisa aconteceu de errado");
+}
+
+/**
+ * auxiliar
+ * @param {Array<string>} stack 
+ * @param {Array<string>} simplified  
+ */
+function reduce(stack, simplified) {
+    while(stack.top() && !isOpen(stack.top())) {
+        simplified.push(stack.pop());
+    }
+    stack.pop();
+}
+/**
+ * auxiliar
+ * @param {Array<string>} stack
+ * @param {Array<string>} simplified 
+ */
+function reduceOperator(next, stack, simplified) {
+    while(stack.top() && isOperator(stack.top())
+        && ((next == "+" || next == "-") && (stack.top() == "*" || stack.top("/")))) {
+            simplified.push(stack.pop());
+    }
+    stack.push(next);
+}
+
+/**
+ * 
+ * @param {Array<string>} stack elementos no padrão (A op B)
+ * @returns {number}  resultado da operação aritmética com pilha atualizada
+ */
+function simpleReduce(stack) {
+    let curr = stack.pop();
+    let b = stack.pop();
+    let op = stack.pop();
+    let a = stack.pop();
+    stack.pop();
+    
+    if(!isNumber(a) || !isNumber(b) || !isOperator(op) || !isClose(curr))
+        throw Error("gramática errada");
+    else
+        return apply(a, b, op);   
+}
+
+
+/**
+ * config e métodos auxiliares
+ *
+ */
+Array.prototype.top = function() { 
+    return this[this.length-1]; 
+}
+Array.prototype.head = function() { 
+    return this[0]; 
+}
+Array.prototype.next = function() {
+    return this[++this.current];
+};
+Array.prototype.prev = function() {
+    return this[--this.current];
+};
+String.prototype.intAt = function(index) {
+    let num = "", string = this;
+    for(i = index; !isNaN(string[i]); i++)
+        num = num.concat(string[i]);
+    return { get: parseInt(num), lastIndex: i-1 };
+}
+String.prototype.floatAt = function(index) {
+    let num = ""; string = this;
+    for(i = index; !isNaN(string[i]) || string[i] == '.'; i++)
+        num = num.concat(string[i]);
+    if(isNaN(num)) throw Error("número inválido");
+    return { get: parseFloat(num), lastIndex: i-1 };
+}
+function isOperator(value) {
+    return ['+', '-', '*', '/'].includes(value);
+}
+function isNumber(value) {
+    return !isWhiteSpace(value) && !isNaN(value);
+}
+function isComma(value) {
+    return value == '.' || value == ',';
+}
+function isOpen(value) {
+    return value == '(';
+}
+function isClose(value) {
+    return value == ')';
+}
+function isWhiteSpace(value) {
+    return value === " ";
+}
+function isEnd(value) {
+    return value == undefined;
+}
+/**
+ * 
+ * @param {number} a operando A
+ * @param {number} b operando B
+ * @param {string} op operador aritmético + - * ou /
+ * @returns {number} resultado da operação aritmética
+ */
+const apply = function(a, b, op) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch(op) {
+        case '+':
+            return a+b;
+            break;
+        case '-':
+            return a-b;
+            break;
+        case '*':
+            return a*b;
+            break;
+        case '/':
+            if(b != 0) return a/b;
+            else throw Error("não pode dividir por zero");
+            break;
+        default:
+            throw Error("operador não suportado");
+            break;
+    }
+}
+
+/**
+ * essa função só serve como tratamento da entrada via terminal quando enviado como string 
+ * @param {string} input string em forma de lista simplificada de uma dada equação
+ * @returns {number} resultado da equação
+ */
+const applyCompute = function(input) {
+    input = input.split(",").map(token => token.trim());
+    input[0] = (input.head()[0] == "[" ? input.head().slice(1,input.head().length) : input.head());
+    input[input.length-1] = (input.top()[input.top().length-1] == "]" ? input.top().slice(0, input.top().length-2) : input.top());
+    input = input.map(token => token = token.replace(/'/g, "")) .map(token => token.trim());
+    return compute(input);
+}
+
+/**
+ * 
+ * @param {Array<string>} simp forma simplificada da equação
+ * @returns {number} resultado da equação
+ */
+function compute(simp) {
+    let aux = [];
+    while(simp.length != 0) {
+        if(isOperator(simp.head())) {
+            let b = aux.pop();
+            let a = aux.pop();
+            let op = simp.shift();
+            aux.push(apply(a,b,op));
+        } else {
+            aux.push(simp.shift());
         }
+    }
+    if (aux.length == 1 ) return aux[0];
+    else throw Error("alguma coisa aconteceu de errado");
+}
 
-        if (stack.length == 1 ) return stack[0];
-        else throw Error("alguma coisa aconteceu de errado");
-    }
-
-    /**
-     * 
-     * @param {Array<string>} stack elementos no padrão (A op B)
-     * @returns {number}  resultado da operação aritmética com pilha atualizada
-     */
-    function simpleReduce(stack) {
-        let curr = stack.pop();
-        let b = stack.pop();
-        let op = stack.pop();
-        let a = stack.pop();
-        stack.pop();
-        
-        if(!isNumber(a) || !isNumber(b) || !isOperator(op) || !isClose(curr))
-            throw Error("gramática errada");
-        else
-            return apply(a, b, op);   
-    }
-
-    Array.prototype.top = function() { 
-        return this[this.length-1]; 
-    }
-    String.prototype.intAt = function(index) {
-        let num = "", string = this;
-        for(i = index; !isNaN(string[i]); i++)
-            num = num.concat(string[i]);
-        return { get: parseInt(num), lastIndex: i-1 };
-    }
-    String.prototype.floatAt = function(index) {
-        let num = ""; string = this;
-        for(i = index; !isNaN(string[i]) || string[i] == '.'; i++)
-            num = num.concat(string[i]);
-        if(isNaN(num)) throw Error("número inválido");
-        return { get: parseFloat(num), lastIndex: i-1 };
-    }
-    function isOperator(value) {
-        return ['+', '-', '*', '/'].includes(value);
-    }
-    function isNumber(value) {
-        return !isWhiteSpace(value) && !isNaN(value);
-    }
-    function isComma(value) {
-        return value == '.' || value == ',';
-    }
-    function isOpen(value) {
-        return value == '(';
-    }
-    function isClose(value) {
-        return value == ')';
-    }
-    function isWhiteSpace(value) {
-        return value === " ";
-    }
-    function isEnd(value) {
-        return value == undefined;
-    }
-    const apply = function(a, b, op) {
-        a = parseFloat(a);
-        b = parseFloat(b);
-        switch(op) {
-            case '+':
-                return a+b;
-                break;
-            case '-':
-                return a-b;
-                break;
-            case '*':
-                return a*b;
-                break;
-            case '/':
-                if(b != 0) return a/b;
-                else throw Error("não pode dividir por zero");
-                break;
-            default:
-                throw Error("operador não suportado");
-                break;
-        }
-    }
-
-    /*
-        function simpleReduce(stack) {
-        if(isClose(stack.top()))
-            stack.pop();
-        let b = stack.pop();
-        let op = stack.pop();
-        let a = stack.pop();
-        let result = apply(a, b, op);
-        
-        if(isOpen(stack.top()))
-            stack.pop();
-        else {
-            stack.push(result);
-            return simpleReduce(stack);
-        }
-        
-        if(!isNumber(a) || !isNumber(b) || !isOperator(op))
-            throw Error("gramática errada");
-        else
-            return result;   
-    }
-    */
-
-module.exports = { simpleParse, apply };
+// exports
+module.exports = { parse, simpleParse, apply, applyCompute };
